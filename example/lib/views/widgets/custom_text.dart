06202364f75931c293edc7634b21b1bf95d5a571
Codefarmer <abadasamuelosp@gmail.com>
2024-07-04 00:27:02 -0700

    chore: add ci workflow

--- example/lib/views/widgets/custom_text.dart
+++ example/lib/views/widgets/custom_text.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:linkify/linkify.dart';
 import 'package:url_launcher/url_launcher.dart';
 
+// ignore: must_be_immutable
 class CustomText extends StatelessWidget {
   final String text;
   final double? fontSize;
